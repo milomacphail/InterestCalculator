763479566f4abddbfcebfcc6b20844b7e6f80ad9
milomacphail <milomacphail@gmail.com>
2019-09-05 19:59:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InterestCalculator
+{
+    class Interest
+    {
+        public int InterestOverYear(int balance, DateTime date, float annualInterestRate)
+        {
+            int year = date.Year;
+            int interestPeriod = year - DateTime.Now.Year;
+
+            int monthlyInterestRate = (int)annualInterestRate / 12;
+
+            int interestAccumulated = (monthlyInterestRate * balance) * interestPeriod;
+
+            return interestAccumulated;
+        }
+
+        public int InterestOverMonths(int balance, int monthsPassed, float annualInterestRate)
+        {
+            int monthlyInterestRate = (int)annualInterestRate / 12;
+
+            int monthlyInterestAccumluated = (monthlyInterestRate * balance) * monthsPassed;
+
+            int interestPlusBalance = monthlyInterestAccumluated + balance;
+
+            return interestPlusBalance;
+
+        }
+
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interest interestOverYear = new Interest();
+            int showInterest = interestOverYear.InterestOverYear(2000, 1, 0.5f);
+            Console.WriteLine(showInterest);
+        }
+    }
+}
